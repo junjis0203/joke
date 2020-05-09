9510eaa92246c1255510674a65dad15fd174fa3e
Junji Sawada <junjis0203@gmail.com>
2020-05-10 07:38:56 +0900

    Implement output check.

--- test.js
+++ test.js
@@ -2,15 +2,61 @@ import JokeEngine from 'joke';
 import fs from 'fs';
 import path from 'path';
 
-async function check(checkProgramDir) {
-    const dir = await fs.promises.opendir(checkProgramDir);
-    for await (const dirent of dir) {
-        const sourceFile = dirent.name;
+function runAndVerify(checkProgramDir, sourceFile) {
+    return new Promise(async (resolve, reject) => {
+        // swap console to vewrify program output
+        const trueConsole = console;
+        const fakeStdoutFile = `${sourceFile}.stdout`;
+        const fakeStdout = fs.createWriteStream(fakeStdoutFile);
+        const fakeConsole = new trueConsole.Console({stdout: fakeStdout});
+        console = fakeConsole;
+
         const sourcePath = path.join(checkProgramDir, sourceFile);
         const data = await fs.promises.readFile(sourcePath, 'utf8');
+        let ok = true;
         try {
             const joke = new JokeEngine();
             joke.run(sourcePath, data);
+        } catch(e) {
+            ok = false;
+            reject(e);
+        } finally {
+            console = trueConsole;
+        }
+
+        // verify whether output is expect
+        fakeStdout.on('finish', async () => {
+            if (ok) {
+                const expectPath = path.join(checkProgramDir, `${sourceFile}.expect`);
+                let expect = await fs.promises.readFile(expectPath, 'utf8');
+                let actual = await fs.promises.readFile(fakeStdoutFile, 'utf8');
+        
+                // consider CRLF
+                expect = expect.replace('\r\n', '\n');
+                actual = actual.replace('\r\n', '\n');
+                if (expect == actual) {
+                    resolve();
+                } else {
+                    reject(new Error('Program output is not expected'));
+                }
+            }
+
+            fs.promises.unlink(fakeStdoutFile)
+        });
+        fakeStdout.end();
+    });
+}
+
+async function check(checkProgramDir) {
+    const dir = await fs.promises.opendir(checkProgramDir);
+    for await (const dirent of dir) {
+        if (!dirent.name.endsWith('.js')) {
+            continue;
+        }
+
+        const sourceFile = dirent.name;
+        try {
+            await runAndVerify(checkProgramDir, sourceFile);
             console.log(`${sourceFile}: OK`);
         } catch(e) {
             console.log(`${sourceFile}: NG`);
