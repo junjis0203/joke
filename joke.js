f6ff719bd9f2779d89786013b48477a988e17b97
Junji Sawada <junjis0203@gmail.com>
2020-05-10 18:58:49 +0900

    Add debug flag for dumping analysis result.

--- joke.js
+++ joke.js
@@ -6,7 +6,22 @@ if (!process.argv[2]) {
     process.exit(0);
 }
 
-const sourceFile = process.argv[2];
+let debug = false;
+let argc = 2;
+while (true) {
+    if (process.argv[argc][0] != '-') {
+        break;
+    }
+
+    switch (process.argv[argc]) {
+    case '-d':
+        debug = true;
+        break;
+    }
+    argc++;
+}
+
+const sourceFile = process.argv[argc];
 fs.readFile(sourceFile, 'utf8', (err, data) => {
     if (err) {
         console.error(err);
@@ -14,7 +29,7 @@ fs.readFile(sourceFile, 'utf8', (err, data) => {
     }
 
     try {
-        const joke = new JokeEngine();
+        const joke = new JokeEngine(debug);
         joke.run(sourceFile, data);
     } catch (e) {
         console.error(`${e.name}: ${e.message}`);
