2fe910e710a43ce4aaa3930e2a5ad2d1060bb376
Junji Sawada <junjis0203@gmail.com>
2020-05-24 16:34:39 +0900

    Implement simple REPL.

--- joke.js
+++ joke.js
@@ -1,15 +1,11 @@
 import JokeEngine from 'joke';
 import fs from 'fs';
-
-if (!process.argv[2]) {
-    console.log(`Usage: node joke.js sourceFile`);
-    process.exit(0);
-}
+import readline from 'readline';
 
 let debug = false;
 let argc = 2;
 while (true) {
-    if (process.argv[argc][0] != '-') {
+    if (!process.argv[argc] || process.argv[argc][0] != '-') {
         break;
     }
 
@@ -17,21 +13,57 @@ while (true) {
     case '-d':
         debug = true;
         break;
+    case '-h':
+        console.log('Usage: node joke.js [options] [sourceFile]\n');
+        console.log('Options:');
+        console.group();
+        console.log('-d\tEnable debug mode (dump compiler output)')
+        console.groupEnd();
+        process.exit(0);
     }
     argc++;
 }
 
-const sourceFile = process.argv[argc];
-fs.readFile(sourceFile, 'utf8', (err, data) => {
-    if (err) {
-        console.error(err);
-        return;
-    }
+function runSourceFile(sourceFile) {
+    fs.readFile(sourceFile, 'utf8', (err, data) => {
+        if (err) {
+            console.error(err);
+            return;
+        }
+    
+        try {
+            const joke = new JokeEngine(debug);
+            joke.run(sourceFile, data);
+        } catch (e) {
+            console.error(`${e.name}: ${e.message}`);
+        }
+    });
+}
 
-    try {
-        const joke = new JokeEngine(debug);
-        joke.run(sourceFile, data);
-    } catch (e) {
-        console.error(`${e.name}: ${e.message}`);
-    }
-});
+function executeRepl() {
+    const joke = new JokeEngine(debug);
+    const rl = readline.createInterface({
+        input: process.stdin,
+        output: process.stdout,
+        prompt: '> '
+    });
+    rl.on('line', (line) => {
+        try {
+            joke.run('<stdin>', line);
+        } catch (e) {
+            console.error(`${e.name}: ${e.message}`);
+        }
+        rl.prompt();
+    });
+    rl.on('close', () => {
+        process.exit(0);
+    })
+    rl.prompt();
+}
+
+const sourceFile = process.argv[argc];
+if (sourceFile) {
+    runSourceFile(sourceFile);    
+} else {
+    executeRepl();
+}
