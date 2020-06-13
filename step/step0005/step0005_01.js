f06b1d20250c9edc8068d0010850e9a29a36c57c
Junji Sawada <junjis0203@gmail.com>
2020-06-13 14:06:40 +0900

    JOKE step5-1 and 5-2.

---
+++ step/step0005/step0005_01.js
@@ -0,0 +1,10 @@
+/*
+Relational operator.
+*/
+const a = 5, b = 3;
+console.log(a == b);
+console.log(a != b);
+console.log(a >  b);
+console.log(a >= b);
+console.log(a <  b);
+console.log(a <= b);
---
+++ step/step0005/step0005_02.js
@@ -0,0 +1,7 @@
+/*
+Abstract and strict equality.
+*/
+console.log(5 == 5);
+console.log(5 === 5);
+console.log(5 == '5');
+console.log(5 === '5');
