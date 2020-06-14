b1602eea72615e17c6f161fe38875ee646394a54
Junji Sawada <junjis0203@gmail.com>
2020-06-14 12:54:00 +0900

    JOKE step6-1 and 6-2.

---
+++ step/step0006/step0006_01.js
@@ -0,0 +1,12 @@
+function test(a, b) {
+    console.log('start');
+    if (a > b) {
+        console.log('a is greather than b');
+    } else {
+        console.log('a is not greather than b');
+    }
+    console.log('end');
+}
+
+test(5, 3);
+test(3, 3);
---
+++ step/step0006/step0006_02.js
@@ -0,0 +1,15 @@
+function test(a, b) {
+    console.log('start');
+    if (a > b) {
+        console.log('a is greather than b');
+    } else if (a == b) {
+        console.log('a is equal to b');
+    } else {
+        console.log('a is less than b');
+    }
+    console.log('end');
+}
+
+test(5, 3);
+test(3, 3);
+test(3, 5);
